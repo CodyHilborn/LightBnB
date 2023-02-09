b96c43e5e9cbf821d90702886aa947f23af18013
Cody Hilborn <codyhilborn11@gmail.com>
2023-02-09 20:11:40 +0000

    Refactor getAllProperties function to accept search input

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -122,7 +122,7 @@ const getAllReservations = function(guest_id, limit = 10) {
   return pool
     .query(queryString, queryParams)
     .then(result => {
-      console.log(result.rows);
+      // console.log(result.rows);
       return result.rows;
     })
     .catch(err => {
@@ -144,12 +144,57 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
 
-  const queryString = `
-    SELECT * FROM properties LIMIT $1
+  const queryParams = [];
+
+  let queryString = `
+    SELECT properties.*, AVG(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
     `;
 
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length}`;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `WHERE owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`);
+    queryString += `
+      AND cost_per_night > $${queryParams.length}
+      `;
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`);
+    queryString += `
+      AND cost_per_night < $${queryParams.length}
+      `;
+  }
+
+  queryString += `
+    GROUP BY properties.id
+    `;
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  queryParams.push(limit);
+  queryString += `
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+    `;
+
+  // console.log(queryString, queryParams);
+
   return pool
-    .query(queryString, [limit])
+    .query(queryString, queryParams)
     .then(result => {
       // console.log(result.rows);
       return result.rows;
